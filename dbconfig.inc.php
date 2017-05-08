074aad7f582a213f636a879c8b7153eb12c7d1f0
AmerAref <a.aref@studenti.gobettivolta.gov.it>
2017-05-08 18:26:33 +0200

    Create dbconfig.inc.php

---
+++ dbconfig.inc.php
@@ -0,0 +1,15 @@
+<?php
+/*
+  blocco dei parametri di connessione
+*/
+
+// db driver
+$driver = "mysql";
+// nome di host
+$host = "127.0.0.1";
+// nome del database
+$db = "libreria";
+// username dell'utente in connessione
+$user = "root";
+// password dell'utente
+$password = "";
